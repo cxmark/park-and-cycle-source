353f3e2b945cd243108c3c7ad5d848bde3d217b6
Dan Hilton <daniel.hilton@gmail.com>
2014-03-18 13:40:00 +0000

    Add in display calls and route storage.

--- ui/js/app/app.js
+++ ui/js/app/app.js
@@ -7,11 +7,28 @@ $(document).ready(function() {
         lng: -2.365556,
     });
 
+    // Given a route, display it as an option to the user
+    function show_route_option(route) {
+        display_route(route)
+
+    }
+
+    function display_route(route) {
+        map.drawRoute({
+            origin: route.origin,
+            destination: route.destination,
+            travelMode: 'bicycling',
+            strokeColor: '#131540',
+            strokeOpacity: 0.3,
+            strokeWeight: 3
+        })
+    }
+
 
     // On click grab the values & geoCode 
     $('#submit-options').click(function() {
-        var start = $('#start').val();
-        var dest = $('#destination').val();
+        var start = "bs57xt" //$('#start').val();
+        var dest = "bs14nd" //$('#destination').val();
 
         GMaps.geocode({
             address: start,
@@ -24,13 +41,16 @@ $(document).ready(function() {
                         callback: function(results, status) {
                             if (status == 'OK') {
                                 var dest_pnt = results[0].geometry.location;
-                                map.drawRoute({
+                                map.getRoutes({
                                     origin: [start_pnt['k'], start_pnt['A']],
                                     destination: [dest_pnt['k'], dest_pnt['A']],
-                                    strokeColor: '#131540',
                                     travelMode: 'bicycling',
-                                    strokeOpacity: 0.5,
-                                    strokeWeight: 3
+                                    waypoints: [{
+                                        'location': "BS4 5LR"
+                                    }],
+                                    callback: function(results, status) {
+                                        show_route_option(results);
+                                    }
                                 });
                             }
                         }
