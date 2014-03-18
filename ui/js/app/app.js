26452124acd07fc9eb448972be5ab308636b8db1
Dan Hilton <daniel.hilton@gmail.com>
2014-03-18 13:16:17 +0000

    Routing working done for first example, next add waypoints of park and ride

--- ui/js/app/app.js
+++ ui/js/app/app.js
@@ -1,4 +1,44 @@
-$(function(){
+var map;
+$(document).ready(function() {
+    map = new GMaps({
+        div: '#mapHolder',
+        zoom: 12,
+        lat: 51.379444,
+        lng: -2.365556,
+    });
+
+
+    // On click grab the values & geoCode 
+    $('#submit-options').click(function() {
+        var start = $('#start').val();
+        var dest = $('#destination').val();
+
+        GMaps.geocode({
+            address: start,
+            callback: function(results, status) {
+                if (status == 'OK') {
+                    var start_pnt = results[0].geometry.location;
+                    console.log(start_pnt);
+                    GMaps.geocode({
+                        address: dest,
+                        callback: function(results, status) {
+                            if (status == 'OK') {
+                                var dest_pnt = results[0].geometry.location;
+                                map.drawRoute({
+                                    origin: [start_pnt['k'], start_pnt['A']],
+                                    destination: [dest_pnt['k'], dest_pnt['A']],
+                                    strokeColor: '#131540',
+                                    travelMode: 'bicycling',
+                                    strokeOpacity: 0.5,
+                                    strokeWeight: 3
+                                });
+                            }
+                        }
+                    })
+                }
+            }
+        });
+    });
 
 
 
